b9a1e95c6f8a311c8f8298747efbe9fe3173b803
jegoi <jeremy.goimard@gmail.com>
2019-04-16 15:32:50 -0400

    Add projects and separate projects from forms (fulcrum apps) and their records

--- projects.py
+++ projects.py
@@ -4,25 +4,121 @@
 from parameters import *
 import parameters as PA
 import tools as TO
+import tools_fulcrum_api as TOFA
 # System
 import os
+
+def get_ProjectsPath():
+  projectsPath = PA.FulcrumPath+'Projects'
+  TO.create_directory(projectsPath)
+  return projectsPath
 
 ##############################################
 # Projects
 ##############################################
+
+class Projects():
+  def __init__(self):
+    self.projects = []
+    self.idName = {}
+  
+  def add_project(self,project):
+    self.projects.append(project)
+    self.idName[project.id] = project.name
+    
+  def get_project_idName(self):
+    return self.idName
+    
+  def get_projects_size(self):
+    return len(self.projects)
+    
+  
+class Project():
+  def __init__(self,created_at = "", description = "", ID = "", name = "", updated_at = ""):
+    self.created_at = created_at
+    self.description = description
+    self.id = ID
+    self.file_name = ""
+    self.name = name
+    self.name_cleaned = ""
+    self.updated_at = updated_at
+  
+  def set_NameCleaned(self):
+    self.name_cleaned = TO.clean_name(self.name)
+  
+  def set_file_name(self):
+    self.file_name = get_ProjectsPath()+'/'+self.name_cleaned+'.json'
+  
+#
+# Project(s) From Json
+#
+def create_projects_from_json(projectsJson):
+  ps = Projects()
+  for projectJson in projectsJson:
+    project = create_project_from_json(projectJson)
+    ps.add_project(project)
+  return ps
+
+def create_project_from_json(projectJson):
+  project = Project(projectJson['created_at'], projectJson['description'], projectJson['id'], projectJson['name'], projectJson['updated_at'])
+  project.set_NameCleaned()
+  project.set_file_name()
+  return project
+    
+#
+# Backup Projects
+#
+def backup_projects_from_Fulcrum():
+  bName = TO.get_file_basename('Projects')
+  if not os.path.exists(bName):
+    os.makedirs(bName)
+  
+  projectsJson = TOFA.get_projects()
+  if projectsJson:
+    fname = PA.FulcrumPath+'all_projects.json'
+    TO.save_in_json_file(fname,projectsJson)
+
+    ps = create_projects_from_json(projectsJson)
+    for project in ps.projects[:]:
+      fname = project.file_name
+      TO.save_in_json_file(fname,project.__dict__)
+    return ps
+  else:
+    return Project()
+
+#
+# Load Project(s)
+#
+def get_project_name_from_id(project_id):
+  projects_idName = load_projects_idName()
+  if project_id in projects_idName:
+    return projects_idName[project_id]
+  else:
+    return ""
+
+def load_projects_idName():
+  ps = load_projects()
+  return ps.get_project_idName()
+
 def load_projects():
-  projectsPath = FulcrumPath+'Projects/'
-  # list files in directory
-  projects = {}
-  projectsFile = TO.get_files_from_path(projectsPath)
+  ps = load_backuped_projects()
+  if ps.get_projects_size() < 1:
+    ps = backup_projects_from_Fulcrum()
+  return ps
+
+def load_backuped_projects():
+  ps = Projects()
+  projectsFile = TO.get_files_from_path(get_ProjectsPath()+'/')
   for projectFile in projectsFile:
-    projectJson = TO.load_json_file(projectFile)['project']
-    projectName = projectJson['name']
-    projectID   = projectJson['id']
-    projects[projectID] = projectName
-  return projects
+    projectJson = TO.load_json_file(projectFile)
+    if 'id' in projectJson:
+      project = create_project_from_json(projectJson)
+      ps.add_project(project)
+  return ps
 
-
+#
+# Website Project(s) directories (with symbolic links)
+#
 def create_project_website_view_directories(projects):
   for project in projects.values():
     create_project_directories(project)
@@ -47,4 +143,18 @@ def create_symbolic_links(projectName):
         dest = PA.ProjectWebsitePath+projectName+'/'+directory+'/'+subDirectory
         if not os.path.exists(dest):
           os.symlink(src,dest)
+
+#
+# Records and Project
+#
+def extract_projects_ID_from_records(records):
+  ps = Projects()
+  for record in records:
+    if 'project_id' in record and record['project_id']:
+      project_id = record['project_id']
+      if project_id not in ps.idName:
+        projectJson = find_project_from_project_ID(project_id)
+        project = create_project_from_json(projectJson)
+        ps.add_project(project)
+  return projectsID
 

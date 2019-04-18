074535a99631bcabaf5f9e792fdd809ba131bbc5
jegoi <jeremy.goimard@gmail.com>
2019-04-18 16:50:36 -0400

    Change records id, backup records, integrate forms and projects for faster processing

---
+++ fulcrum_backup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import records as RE
+import time
+
+'''
+Usage:
+python backup_fulcrum.py [-h] [-a FulcrumApiKey] [-f FORMS [FORMS ...]]
+                                [-p PATH]
+
+Options Fulcrum Backup.
+optional arguments:
+  -a FulcrumApiKey, --FulcrumApiKey FulcrumApiKey
+                        Need a fulcrum Api key
+  -f FORMS [FORMS ...], --forms FORMS [FORMS ...]
+                        Select specific forms by name or id. Example: -f Plots
+                        Sites 'Bulk Leaf Samples' 'Vegetation Surveys: Herbs
+                        and Shrubs'
+  -p PATH, --path PATH  Choose a directory for backup
+'''
+##############################################
+# MAIN FUNCTION
+##############################################
+def main():
+  RE.backup_records_from_forms()
+
+##############################################
+# MAIN
+##############################################
+if __name__ == '__main__': 
+  main ()
+
--- records.py
+++ records.py
@@ -3,13 +3,15 @@
 # Local Modules
 import parameters as PA
 import forms as FO
-import projects as PO
+import photos as PH
+import projects as PR
 import tools as TO
 import tools_fulcrum_api as TOFA
 import logs as LO
 
 # System
-import sys, copy
+import sys, copy, time
+import multiprocessing as mp
 
 ##############################################
 # Record
@@ -48,7 +50,7 @@ class Records(object):
   
   def add_record(self,record_raw):
     self.records.append(record_raw)
-    self.recordsDict[record_raw.ID]=record_raw
+    self.recordsDict[record_raw.id]=record_raw
   
   def clean_records(self):
     self.records = []
@@ -76,7 +78,7 @@ class Record(object):
     self.form_name = RformName
     self.form_values = RformValues
     self.horizontal_accuracy = RhorizontalAccuracy
-    self.ID = RID
+    self.id = RID
     self.latitude = Rlatitude
     self.longitude = Rlongitude
     self.project_id = RprojectId
@@ -95,10 +97,10 @@ class Record(object):
      
 
   def __str__(self):
-    return '>{}'.format([self.altitude, self.assigned_to, self.assigned_to_id, self.client_created_at, self.client_updated_at, self.course, self.created_at, self.created_by, self.created_by_id, self.created_duration, self.created_location, self.edited_duration, self.form_id, self.horizontal_accuracy, self.ID, self.latitude, self.longitude, self.project_id, self.speed, self.status, self.updated_at, self.updated_by, self.updated_by_id, self.updated_duration, self.updated_location, self.version, self.vertical_accuracy])
+    return '>{}'.format([self.altitude, self.assigned_to, self.assigned_to_id, self.client_created_at, self.client_updated_at, self.course, self.created_at, self.created_by, self.created_by_id, self.created_duration, self.created_location, self.edited_duration, self.form_id, self.horizontal_accuracy, self.id, self.latitude, self.longitude, self.project_id, self.speed, self.status, self.updated_at, self.updated_by, self.updated_by_id, self.updated_duration, self.updated_location, self.version, self.vertical_accuracy])
 
   def to_csv(self):
-    return [self.altitude, self.assigned_to, self.assigned_to_id, self.client_created_at, self.client_updated_at, self.course, self.created_at, self.created_by, self.created_by_id, self.created_duration, self.created_location, self.edited_duration, self.form_id, self.horizontal_accuracy, self.ID, self.latitude, self.longitude, self.project_id, self.speed, self.status, self.updated_at, self.updated_by, self.updated_by_id, self.updated_duration, self.updated_location, self.version, self.vertical_accuracy]
+    return [self.altitude, self.assigned_to, self.assigned_to_id, self.client_created_at, self.client_updated_at, self.course, self.created_at, self.created_by, self.created_by_id, self.created_duration, self.created_location, self.edited_duration, self.form_id, self.horizontal_accuracy, self.id, self.latitude, self.longitude, self.project_id, self.speed, self.status, self.updated_at, self.updated_by, self.updated_by_id, self.updated_duration, self.updated_location, self.version, self.vertical_accuracy]
 
   def to_info(self):
     return []
@@ -107,11 +109,11 @@ class Record(object):
     print type(self).__name__
     
   def is_record(self):
-    if self.ID and self.form_id:
+    if self.id and self.form_id:
       return True
     else:
-      LO.l_debug('The record has no ID ({}) and/or "form_id" ({}). We will not be able to process it.'.format(self.ID,self.form_id))
-      self.add_toLog('The record has no ID ({}) and/or "form_id" ({}). We will not be able to process it.'.format(self.ID,self.form_id))
+      LO.l_debug('The record has no ID ({}) and/or "form_id" ({}). We will not be able to process it.'.format(self.id,self.form_id))
+      self.add_toLog('The record has no ID ({}) and/or "form_id" ({}). We will not be able to process it.'.format(self.id,self.form_id))
       return False
       
   def add_toLog(self, st):
@@ -120,8 +122,8 @@ class Record(object):
 ##############################################
 # Records Functions
 ##############################################
-def create_record_from_json(record_raw):
-  Raltitude, RassignedTo, RassignedToId, RclientCreatedAt, RclientUpdatedAt, Rcourse, RcreatedAt, RcreatedBy, RcreatedById, RcreatedDuration, RcreatedLocation, ReditedDuration, RformId, RformName, RhorizontalAccuracy, RID, Rlatitude, Rlongitude, RprojectId, Rspeed, Rstatus, RupdatedAt, RupdatedBy, RupdatedById, RupdatedDuration, RupdatedLocation, Rversion, RverticalAccuracy, RformValues = ("",)*29
+def create_record_from_json(record_raw,forms=[],projects=[]):
+  Raltitude, RassignedTo, RassignedToId, RclientCreatedAt, RclientUpdatedAt, Rcourse, RcreatedAt, RcreatedBy, RcreatedById, RcreatedDuration, RcreatedLocation, ReditedDuration, RformId, RformName, RhorizontalAccuracy, RID, Rlatitude, Rlongitude, RprojectId, Rspeed, Rstatus, RupdatedAt, RupdatedBy, RupdatedById, RupdatedDuration, RupdatedLocation, Rversion, RverticalAccuracy, RformValues, RprojectName = ("",)*30
   if 'altitude'           in record_raw:  Raltitude = record_raw['altitude']
   if 'assigned_to'        in record_raw:  RassignedTo = record_raw['assigned_to']
   if 'assigned_to_id'     in record_raw:  RassignedToId = record_raw['assigned_to_id']
@@ -136,7 +138,7 @@ def create_record_from_json(record_raw):
   if 'edited_duration'    in record_raw:  ReditedDuration = record_raw['edited_duration']
   if 'form_id'            in record_raw:
     RformId = record_raw['form_id']
-    RformName = FO.get_formName_from_formid(RformId)
+    RformName = FO.get_formName_from_formid(RformId,forms)
   if 'form_values'        in record_raw:  RformValues = record_raw['form_values']
   if 'horizontal_accuracy' in record_raw: RhorizontalAccuracy = record_raw['horizontal_accuracy']
   if 'id'                 in record_raw:  RID = record_raw['id']
@@ -153,30 +155,32 @@ def create_record_from_json(record_raw):
   if 'version'            in record_raw:  Rversion = record_raw['version']
   if 'vertical_accuracy'  in record_raw:  RverticalAccuracy = record_raw['vertical_accuracy']
   
-  record = Record(Raltitude, RassignedTo, RassignedToId, RclientCreatedAt, RclientUpdatedAt, Rcourse, RcreatedAt, RcreatedBy, RcreatedById, RcreatedDuration, RcreatedLocation, ReditedDuration, RformId, RformName, RformValues, RhorizontalAccuracy, RID, Rlatitude, Rlongitude, RprojectId, Rspeed, Rstatus, RupdatedAt, RupdatedBy, RupdatedById, RupdatedDuration, RupdatedLocation, Rversion, RverticalAccuracy)
+  record = Record(Raltitude, RassignedTo, RassignedToId, RclientCreatedAt, RclientUpdatedAt, Rcourse, RcreatedAt, RcreatedBy, RcreatedById, RcreatedDuration, RcreatedLocation, ReditedDuration, RformId, RformName, RformValues, RhorizontalAccuracy, RID, Rlatitude, Rlongitude, RprojectId, Rspeed, Rstatus, RupdatedAt, RupdatedBy, RupdatedById, RupdatedDuration, RupdatedLocation, Rversion, RverticalAccuracy,RprojectName)
   
   if not RID and not RformId:
-    print 'record_raw'
-    print record_raw
-    
+    LO.l_debug('record_raw')
+    LO.l_debug(record_raw)
+  
+  LO.l_debug('RID :{}'.format(RID))
+  
   if not RID and not RformId:
     print('The record is not valid. Record Values:\n{}'.format(record))
     record.isValid=False
   else:
-    is_form_project_enabled = FO.get_projects_enabled_status(record.form_id)
+    is_form_project_enabled = FO.get_projects_enabled_status(record.form_id,forms)
     if is_form_project_enabled:
-      record.project_name = PO.get_project_name_from_id(record.project_id)
-      if not record.project_name:
-        sNoP = 'Form id {} (name: {}), The record id {} is not associated project name with a project name and it has the project id {}.'.format(record.form_id, record.form_name,record.ID,record.project_id)
+      record.project_name = PR.get_project_name_from_id(record.project_id,projects)
+      if record.project_name == "":
+        sNoP = 'Form id {} (name: {}), The record id {} is not associated project name with a project name and it has the project id {}.'.format(record.form_id, record.form_name,record.id,record.project_id)
         LO.l_war(sNoP)
         record.add_toLog(sNoP)
       else:
-        LO.l_debug('Form id {} (name: {}), The record id {} has an associated project name with a project name and it has the project id {}.'.format(record.form_id, record.form_name,record.ID,record.project_id))
+        LO.l_debug('Form id {} (name: {}), The record id "{}" has an associated project name with a project name ({}) and id ({}).'.format(record.form_id, record.form_name, record.id,record.project_name, record.project_id))
     else:
-      LO.l_debug('Form id {} (name: {}) has not its project enabled.'.format(record.form_id, record.form_name,record.ID,record.project_id))
+      LO.l_debug('Form id {} (name: {}) has not its project enabled.'.format(record.form_id, record.form_name,record.id,record.project_id))
     
     if Rstatus and 'deleted' in Rstatus:
-      sdel = 'Form id {} (name: {}), Project id {} (name: {}), the record id {} will not be used because its status is deleted.'.format(record.form_id, record.form_name,record.project_id,record.project_name,record.ID)
+      sdel = 'Form id {} (name: {}), Project id {} (name: {}), the record id {} will not be used because its status is deleted.'.format(record.form_id, record.form_name,record.project_id,record.project_name,record.id)
       LO.l_err(sdel)
       record.add_toLog(sdel)
       record.isValid=False
@@ -197,13 +201,75 @@ def record_from_fileName(fileName):
 def get_list_records_from_file(fileName):
   return TO.load_json_file(fileName)
 
-def get_records_from_list(listRecords):
+def get_records_from_list(listRecords,forms=[],projects=[]):
+  # preload projects and forms if they are empty
+  if len(projects)<1:
+    projects = PR.load_projects()
+  if len(forms)<1:
+    forms = FO.load_forms()
   records = Records()
-  for record_raw in listRecords:
-    record = create_record_from_json(record_raw)
+  for record_raw in listRecords[:]:
+    record = create_record_from_json(record_raw,forms,projects)
     records.add_record(record)
   return records
 
+
+##############################################
+# Backup Records From Forms
+##############################################
+def backup_records_from_forms():
+  PA.set_parameters()
+  LO.create_log("main","",'backup_fulcrum')
+  TOFA.check_fulcrum_version()
+  # Backup projects
+  PR.backup_projects_from_Fulcrum()
+  # Backup Forms (Applications)
+  formsO = FO.load_fulcrum_formsJson()
+  #for form in formsO.forms[:]:
+  #  mp_backup_records_from_form(form)
+  mp_backup_records_from_forms(formsO)
+  TOFA.print_num_of_request()
+
+def mp_backup_records_from_forms(formsO):
+  """
+  This parallelisation of backup_form
+  """
+  # parallelisation here
+  output = mp.Queue()
+  wraps = []
+  pool = mp.Pool(processes=PA.NumberOfProcesses)
+  recordsForm = [pool.apply_async(mp_backup_records_from_form, args=(form,)) for form in formsO.forms[:]]
+  pool.close()
+  pool.join()
+  for r in recordsForm:
+    b = r.get()
+    if b:
+      wraps.append(b)
+  return wraps
+
+def mp_backup_records_from_form(form):
+  formName = form.name_cleaned
+  formID   = form.id
+  start = time.time()
+  if formID:
+  #if formID == 'fd35169b-186c-4548-a5e5-488bd7432152':
+    # Backup records
+    records = backup_records_from_form(form)
+    
+    # Backup records versions
+    if form.script and '/* SAVE VERSIONS */' in form.script:
+      backup_records_versions(form,records)
+    else:
+      LO.l_war("No records versions for {st} will not be saved.\n\
+                If you need to saved records versions for {st}.\n\
+                Please add /* SAVE VERSIONS */ in the top of it's 'data events'\n\
+                ".format(st=formName))
+    
+    # Backup images
+    PH.backup_photos_from_records(form,records)
+    endtime = time.time()
+    print('########################\nTime to backup form {} IS {}\n########################\n'.format(formID,endtime-start))
+    return records
 
 ##############################################
 # Backup Records and Records Version
@@ -234,11 +300,15 @@ def backup_records_versions(form,recs):
   formName = form.name_cleaned
   bName = TO.get_FormsPath()+formName+''
   
+  # preload projects and forms
+  projects = PR.load_projects()
+  forms = FO.load_forms()
+  
   LO.l_debug('size of recs {} before version'.format(len(recs)))
   LO.l_info('Start backup version for the form "{}" with {} records'.format(formName,form.record_count))
   for record in recs.records[:]:
     currentVersion = record.version
-    recordID      = record.ID
+    recordID      = record.id
     fname         = bName+'/versions/'+recordID+'_versions.json'
     recordsHistory= TO.load_json_file(fname)
     versionIsUpToDate = False
@@ -247,7 +317,6 @@ def backup_records_versions(form,recs):
       latestHistoryVersion = get_record_latest_history_version(recordsHistory)
       if (currentVersion == latestHistoryVersion):
         versionIsUpToDate = True
-        recordsHistory = ''
     
     if not versionIsUpToDate and currentVersion > 1:
       recordsHistory = TOFA.get_record_history(recordID)
@@ -256,13 +325,13 @@ def backup_records_versions(form,recs):
       
     if recordsHistory:
       TO.save_in_json_file(fname,recordsHistory)
-      recordsHistoryTemp = get_records_from_list(recordsHistory)
+      recordsHistoryTemp = get_records_from_list(recordsHistory,forms,projects)
       
       # add records versions from backup file to the records history list
       # but without the latest version which is already there
       for recordHistory in recordsHistoryTemp.records[:]:
         if recordHistory.version != currentVersion:
-          LO.l_debug('add_record {} version {}'.format(recordHistory.ID,recordHistory.version))
+          LO.l_debug('add_record {} version {}'.format(recordHistory.id,recordHistory.version))
           recs.add_record(recordHistory)
     
   LO.l_debug('size of recs {} after version'.format(len(recs)))
@@ -285,37 +354,6 @@ def get_record_latest_history_version(records):
     if record['version'] > latestVersion:
       latestVersion = record['version']
   return latestVersion
-
-
-##############################################
-# LOAD RECORDS
-##############################################
-
-# Load records from Webhooks
-##############################################
-def load_webhook_records():
-  records_files = TO.get_files_from_path(PA.FulcrumWebhook+"records/")
-  return get_records_from_files_list(records_files)
-
-# Load records from File Name
-##############################################
-def load_records_from_json(fileName):
-  records_raw = get_list_records_from_file(fileName)
-  return get_records_from_list(records_raw)
-
-# Load records from backup
-# (very similar to previous one)
-##############################################
-def load_records_from_form(form):
-  records_raw = get_list_records_from_file(form.backup_file)
-  return get_records_from_list(records_raw)
-
-# Load records from fulcrum
-##############################################
-def load_records_from_fulcrum(form):
-  records_raw = TOFA.get_fulcrum_records(form.id,form.name_cleaned)
-  return get_records_from_list(records_raw)
-  
 
 ##############################################
 # Update Record with dataname
@@ -364,3 +402,32 @@ def search_for_keys_recu(dictKeysDataName,info):
     for v in info.values():
       search_for_keys_recu(dictKeysDataName,v)
 
+##############################################
+# LOAD RECORDS
+##############################################
+
+# Load records from Webhooks
+##############################################
+def load_webhook_records():
+  records_files = TO.get_files_from_path(PA.FulcrumWebhook+"records/")
+  return get_records_from_files_list(records_files)
+
+# Load records from File Name
+##############################################
+def load_records_from_json(fileName):
+  records_raw = get_list_records_from_file(fileName)
+  return get_records_from_list(records_raw)
+
+# Load records from backup
+# (very similar to previous one)
+##############################################
+def load_records_from_form(form):
+  records_raw = get_list_records_from_file(form.backup_file)
+  return get_records_from_list(records_raw)
+
+# Load records from fulcrum
+##############################################
+def load_records_from_fulcrum(form):
+  records_raw = TOFA.get_fulcrum_records(form.id,form.name_cleaned)
+  return get_records_from_list(records_raw)
+  
